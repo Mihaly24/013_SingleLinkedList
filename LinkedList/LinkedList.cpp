14089140331574a2543ac915d85d579e31cf51ac
Mihaly24 <fairuzk20@gmail.com>
2024-04-25 17:43:44 +0700

    Menambahkan untuk membuat node

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -9,6 +9,20 @@ struct Node {
 
 Node* START = NULL;
 
+void addNote() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+
+
+}
+
 int main()
 {
     
